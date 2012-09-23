0bb6f6841290ceb659c2aa03ffa50b58bfd94587
alelopbes <alelopbes@gmail.com>
2012-09-23 13:51:40 +0200

    slide configuration

--- js/responsiveslides.js
+++ js/responsiveslides.js
@@ -16,9 +16,9 @@
       "auto": true,             // Boolean: Animate automatically, true or false
       "speed": 1000,            // Integer: Speed of the transition, in milliseconds
       "timeout": 4000,          // Integer: Time between slide transitions, in milliseconds
-      "pager": false,           // Boolean: Show pager, true or false
+      "pager": true,           // Boolean: Show pager, true or false
       "nav": true,             // Boolean: Show navigation, true or false
-      "random": false,          // Boolean: Randomize the order of the slides, true or false
+      "random": true,          // Boolean: Randomize the order of the slides, true or false
       "pause": false,           // Boolean: Pause on hover, true or false
       "pauseControls": false,   // Boolean: Pause when hovering controls, true or false
       "prevText": "Previous",   // String: Text for the "previous" button
