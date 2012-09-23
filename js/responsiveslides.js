6709f2bfefa52e0a5824639d074fd57911330d2f
alelopbes <alelopbes@gmail.com>
2012-09-23 14:21:23 +0200

    Slide

--- js/responsiveslides.js
+++ js/responsiveslides.js
@@ -16,11 +16,11 @@
       "auto": true,             // Boolean: Animate automatically, true or false
       "speed": 1000,            // Integer: Speed of the transition, in milliseconds
       "timeout": 4000,          // Integer: Time between slide transitions, in milliseconds
-      "pager": true,           // Boolean: Show pager, true or false
-      "nav": true,             // Boolean: Show navigation, true or false
-      "random": true,          // Boolean: Randomize the order of the slides, true or false
-      "pause": true,           // Boolean: Pause on hover, true or false
-      "pauseControls": true,   // Boolean: Pause when hovering controls, true or false
+      "pager": false,           // Boolean: Show pager, true or false
+      "nav": false,             // Boolean: Show navigation, true or false
+      "random": false,          // Boolean: Randomize the order of the slides, true or false
+      "pause": false,           // Boolean: Pause on hover, true or false
+      "pauseControls": false,   // Boolean: Pause when hovering controls, true or false
       "prevText": "Previous",   // String: Text for the "previous" button
       "nextText": "Next",       // String: Text for the "next" button
       "maxwidth": "",           // Integer: Max-width of the slideshow, in pixels
