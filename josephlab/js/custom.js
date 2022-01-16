f9bec388660670d72462ce10600887f1a5c08a82
Alejandro López Besteiro <alelopbes@gmail.com>
2022-01-16 20:15:53 +0100

    Move josephlab to local storage

---
+++ josephlab/js/custom.js
@@ -0,0 +1,37 @@
+
+// Animsition
+$(document).ready(function() {
+ $(".animsition").animsition({
+  inClass: 'fade-in',
+  outClass: 'fade-out',
+  inDuration: 1500,
+  outDuration: 500,
+  linkElement: '.animsition-link',
+  // e.g. linkElement: 'a:not([target="_blank"]):not([href^=#])'
+  loading: false,
+  loadingParentElement: 'body', //animsition wrapper element
+  loadingClass: 'animsition-loading',
+  loadingInner: '', // e.g '<img src="loading.svg" />'
+  timeout: false,
+  timeoutCountdown: 5000,
+  onLoadEvent: true,
+  browser: [ 'animation-duration', '-webkit-animation-duration'],
+  // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+  // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+  overlay : false,
+  overlayClass : 'animsition-overlay-slide',
+  overlayParentElement : 'body',
+  transition: function(url){ window.location.href = url; }
+  });
+});
+
+// Typeform
+(function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() 
+
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-75771925-1', 'auto');
+  ga('send', 'pageview');
